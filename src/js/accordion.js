e0a5be76c9b01349cc093e626b55569279a69c9c
Stas Radchenko <stenready@mail.com>
2021-06-03 17:16:21 +0300

    change accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,30 +1,30 @@
 import { accordionData } from "./constants";
 
-const wrap = document.querySelector(".questions__wrap");
+// const wrap = document.querySelector(".questions__wrap");
 
-function generateHTML(accData) {
-	const tmp = accData
-		.map((el) => {
-			const isActive = el.active ? "active" : "";
-			return `
-      <li class="accordion__item ${isActive}">
-      <div class="accordion__title">
-        <span class="text">${el.title}</span>
-        <span class="close">+</span>
-        <span class="open">-</span>
-   
-      </div>
-      <div class="accordion__content">${el.text}</div>
-    </li>
-      `;
-		})
-		.join("");
-	const ul = document.createElement("ul");
-	ul.classList.add("accordion");
-	ul.innerHTML = tmp;
-	return ul;
-}
-wrap.appendChild(generateHTML(accordionData));
+// function generateHTML(accData) {
+// 	const tmp = accData
+// 		.map((el) => {
+// 			const isActive = el.active ? "active" : "";
+// 			return `
+//       <li class="accordion__item ${isActive}">
+//       <div class="accordion__title">
+//         <span class="text">${el.title}</span>
+//         <span class="close">+</span>
+//         <span class="open">-</span>
+
+//       </div>
+//       <div class="accordion__content">${el.text}</div>
+//     </li>
+//       `;
+// 		})
+// 		.join("");
+// 	const ul = document.createElement("ul");
+// 	ul.classList.add("accordion");
+// 	ul.innerHTML = tmp;
+// 	return ul;
+// }
+// wrap.appendChild(generateHTML(accordionData));
 
 const accardion = document.querySelectorAll(".accordion__item");
 accardion.forEach((el) => el.addEventListener("click", accordionHandler));
